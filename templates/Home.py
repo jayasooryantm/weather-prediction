2cd219b88f1263c6a2f8e6bc2d124ced92f16659
Jayasooryan TM <jayasooryan.tm@outlook.com>
2024-09-03 22:18:03 +0530

    Update Home.py

--- templates/Home.py
+++ templates/Home.py
@@ -10,15 +10,15 @@ st.set_page_config(
     page_icon="🌦️"
 )
 
-model_parameters = {
-  "input_dim": 14,
-  "output_dim": 1,
-  "hidden_dim": 20,
-  "learning_rate": 0.001,
-  "batch_size": 100,
-  "num_batches": 62,
-  "num_epochs": 100
-}
+# model_parameters = {
+#   "input_dim": 14,
+#   "output_dim": 1,
+#   "hidden_dim": 20,
+#   "learning_rate": 0.001,
+#   "batch_size": 100,
+#   "num_batches": 62,
+#   "num_epochs": 100
+# }
 
 class WeatherModel(nn.Module):
     def __init__(self, parameters):
@@ -63,6 +63,7 @@ class WeatherModel(nn.Module):
 
 weather_types = { 'NA': 'Not available', '-1': 'Trace rain', '0': 'Clear night', '1': 'Sunny day', '2': 'Partly cloudy (night)', '3': 'Partly cloudy (day)', '4': 'Not used', '5': 'Mist', '6': 'Fog', '7': 'Cloudy', '8': 'Overcast', '9': 'Light rain shower (night)', '10': 'Light rain shower (day)', '11': 'Drizzle', '12': 'Light rain', '13': 'Heavy rain shower (night)', '14': 'Heavy rain shower (day)', '15': 'Heavy rain', '16': 'Sleet shower (night)', '17': 'Sleet shower (day)', '18': 'Sleet', '19': 'Hail shower (night)', '20': 'Hail shower (day)', '21': 'Hail', '22': 'Light snow shower (night)', '23': 'Light snow shower (day)', '24': 'Light snow', '25': 'Heavy snow shower (night)', '26': 'Heavy snow shower (day)', '27': 'Heavy snow', '28': 'Thunder shower (night)', '29': 'Thunder shower (day)', '30': 'Thunder'}
 compass_directions_map = {1: 'N', 2: 'NNE', 3: 'NE', 4: 'ENE', 5: 'E', 6: 'ESE', 7: 'SE', 8: 'SSE', 9: 'S', 10: 'SSW', 11: 'SW', 12: 'WSW', 13: 'W', 14: 'WNW', 15: 'NW', 16: 'NNW'}
+
 MODEL_PATH = "artifacts/model_trainer/models/model.pth"
 
 
@@ -101,7 +102,7 @@ col1.metric("Pressure", str(round(pressure.item())) + " hpa")
 col2.metric("Wind Speed", str(round(wind_speed.item())) + " Mph")
 col2.metric("Temperature", str(round(temperature.item())) + " °C")
 col3.metric("Visibility", str(round(visibility.item())) + " m")
-col3.metric("Weather Type", str(round(weather_type.item())))
+col3.metric("Weather Type", weather_types[str(round(weather_type.item()))])
 
 st.divider()
 
